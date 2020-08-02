f9704dec31ee08f99c6c688638e4be734722e321
Matthew Daffern <daffernmatthew.gmail.com>
2020-08-01 22:39:26 -0700

    onto testing, finally

--- test.py
+++ test.py
@@ -26,7 +26,52 @@ config['site']['firewall']['remote_or_local_script'] = 'remote'
 
 config['site']['firewall']['hostname'] = '127.0.0.1'
 
-print(config)
+config['site']['server']['username'] = 'lol'
+config['site']['server']['password'] = 'lol'
+config['site']['server']['config_result'] = ['1',
+                                               '2',
+                                               '3']
+config['site']['server']['read_config'] = ['1',
+                                             '2',
+                                             '3']
+config['site']['server']['write_config'] = ['1',
+                                              '2',
+                                              '3']
+
+config['site']['server']['temporary_write_config'] = ['1',
+                                                        '2',
+                                                        '3']
+config['site']['server']['remote_or_local_script'] = 'remote'
+
+config['site']['server']['hostname'] = '127.0.0.1'
+
+config['site']['switch']['username'] = 'lol'
+config['site']['switch']['password'] = 'lol'
+config['site']['switch']['config_result'] = ['1',
+                                               '2',
+                                               '3']
+config['site']['switch']['read_config'] = ['1',
+                                             '2',
+                                             '3']
+config['site']['switch']['write_config'] = ['1',
+                                              '2',
+                                              '3']
+
+config['site']['switch']['temporary_write_config'] = ['1',
+                                                        '2',
+                                                        '3']
+config['site']['switch']['remote_or_local_script'] = 'remote'
+
+config['site']['switch']['hostname'] = '127.0.0.1'
+
+
+
+
 
 json.dump(config, file)
 file.close()
+
+import main as test
+
+result = test.one_or_all_or_new(test.main_menu(), config)
+print(result)
